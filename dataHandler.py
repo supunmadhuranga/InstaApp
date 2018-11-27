767936a9f6ad33980049f89a2caa2d48e22872fc
Anuradhe007 <anu.bab007@gmail.com>
2018-11-28 01:54:00 +0530

    Added part to save commented users of a post.

--- dataHandler.py
+++ dataHandler.py
@@ -17,14 +17,19 @@ class dataHandler:
             self.limit = 100
         self.limit = int(limit)
         atValue = ''
-        fileName = 'C:\\Users\\Name\\Documents\\file.txt'
+        fileName = 'C:\\Users\\Uname\\Documents\\Text.txt'
+        fileNameComments = 'C:\\Users\\Uname\\Documents\\Commenters.txt'
         if int(atMark) == 1:
             atValue = '@'
 			
         postCount = 0
         users_file = open(fileName, 'w+')
+        commenters_file = open(fileNameComments, 'w+')
         posts = self.loader.get_hashtag_posts(hashtag)
         for post in posts:
+            if post.comments > 0:
+                for comment in post.get_comments():
+                    commenters_file.write(atValue + str(comment.owner).split(None)[1] + '\n')
             postUsername = ''
             noError = True
             if not post:
@@ -45,6 +50,7 @@ class dataHandler:
             if postCount % 100 == 0:
                 time.sleep(random.randint(2, 5))
         users_file.close()
+        commenters_file.close()
         self.loader.close()
         print("done")
         
